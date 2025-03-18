69054d9023f72a5b5f79714496263b95221b9117
classyoon <96508736+classyoon@users.noreply.github.com>
2025-03-18 18:32:56 -0400

    Initial Commit

---
+++ InventoryMVVM/InventoryMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  InventoryMVVMApp.swift
+//  InventoryMVVM
+//
+//  Created by Conner Yoon on 3/18/25.
+//
+
+import SwiftUI
+
+@main
+struct InventoryMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InventoryMVVMTests/InventoryMVVMTests.swift
@@ -0,0 +1,17 @@
+//
+//  InventoryMVVMTests.swift
+//  InventoryMVVMTests
+//
+//  Created by Conner Yoon on 3/18/25.
+//
+
+import Testing
+@testable import InventoryMVVM
+
+struct InventoryMVVMTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
